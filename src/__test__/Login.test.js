26de23946521fe313a1c9aed1ea90de82905e6a1
kosugi.y <kosugi.y@tsunagu-grp.jp>
2023-03-28 10:59:39 +0900

    feat: add test for login form

--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -1,52 +1,43 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { type } from "@testing-library/user-event/dist/type";
 import Login, { validateEmail } from "../Login";
 
-describe("Test the Login Component", () => {
-  test("render the form with 2 button", async () => {
+describe("Test Login Component", ()=> {
+  test("test render form with 1 button", async ()=> {
     render(<Login />);
-    const buttonList = await screen.findAllByRole("button");
-    // console.log(buttonList);
-    expect(buttonList).toHaveLength(1); //ボタンが２つあるかテスト
+    const button = await screen.findAllByRole("button");
+    // console.log(button);
+    expect(button).toHaveLength(1);
   });
 
-  test("should be failed on email validation", () => {
-    const testEmail = "shincode.com";
-    //間違っているのが正しい（これはテストなので、わざと間違えさせる）
-    expect(validateEmail(testEmail)).not.toBe(true);
+  test("should be faild on email validation", ()=> {
+    const testemail = "test.com";
+    expect(validateEmail(testemail)).not.toBe(true);
   });
 
-  //電子メールの入力欄は電子メールを受け付ける
-  test("email input field should accept email", () => {
-    render(<Login />);
-    const email = screen.getByPlaceholderText("メールアドレス入力");
-    userEvent.type(email, "shincode");
-    expect(email.value).not.toMatch("shincode@gmail.com");
+  test("should be succeeded on email validation", ()=> {
+    const testemail = "test@test.com";
+    expect(validateEmail(testemail)).toBe(true);
   });
 
-  //ここからpackage.jsonでverboseの設定
-  // テストファイルごとの結果ではなく、テストファイル内の各テストケースごとの結果を表示
-
-  //パスワード欄がパスワードタイプで入力できるかを確認
   test("password input should have type password", () => {
     render(<Login />);
     const password = screen.getByPlaceholderText("パスワード入力");
     expect(password).toHaveAttribute("type", "password");
   });
 
-  test("should be able to submit the form", () => {
+  test("should be able to submit the form", ()=> {
     render(<Login />);
     const submitButton = screen.getByTestId("submit");
     const email = screen.getByPlaceholderText("メールアドレス入力");
     const password = screen.getByPlaceholderText("パスワード入力");
 
-    userEvent.type(email, "shincode@gmail.com");
-    userEvent.type(password, "12345");
+    userEvent.type(email, "test-kosugi@gmail.com");
+    userEvent.type(password, "asdcadfg");
 
     userEvent.click(submitButton);
-
-    const userInfo = screen.getByText("shincode@gmail.com");
+    const userInfo = screen.getByText("test-kosugi@gmail.com");
     expect(userInfo).toBeInTheDocument();
   });
-  //userEventはユーザがブラウザ上で行う挙動をfireEventより正確に再現することができる（値の変化だけでなくkeyDown, keyPress, keyUpなど）ので、特に理由がなければuserEventを使った方がいい。
 });
