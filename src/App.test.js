70e42ef25f8f6290ceffe20c1a8453d1e8334d0c
kosugi.y <kosugi.y@tsunagu-grp.jp>
2023-03-28 11:00:08 +0900

    feat: fix App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("title renders with react in the document", () => {
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/React/i);
+  const linkElement = screen.getByText(/react/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-test("render login component in the document", () => {
-  // const component = render(<App />);
-  const { getByLabelText } = render(<App />);
-  // console.log(component);
-  // const childElem = component.getByLabelText("メールアドレス");
-  const childElem = getByLabelText("メールアドレス");
-  // expect(childElem).toBeInTheDocument(); //子コンポーネントがあるかどうかの判定
-  expect(childElem).toBeTruthy(); //子コンポーネントがあるかどうかの判定
 });
